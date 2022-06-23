88e91aea647166b6eace43034e33baa7df6eb5ea
olegarxo <olegarxo15@gmail.com>
2022-06-23 20:59:00 +0300

    Сделал второе задание

---
+++ Program1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+/*Console.WriteLine("Введите первое число");
+int fitht = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int second = Convert.ToInt32(Console.ReadLine());
+
+if {
+fitht == second * second;
+Console.WriteLine("Первое число является квадратом второго");
+}
+else {
+Console.WriteLine("Первое число не является квадратом второго");
+}
+/*Console.WriteLine("Введите число");
+int some = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Квадрат числа");
+some = some * some;
+Console.WriteLine(some);
+*/
